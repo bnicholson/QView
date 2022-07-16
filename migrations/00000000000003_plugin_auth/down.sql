937df192ebe0f4f98002b0a9fb2a2883b67e19c1
B. Nicholson <b.nicholson@niceng.com>
2022-07-16 19:26:58 -0400

    Added Auth plugin

---
+++ migrations/00000000000003_plugin_auth/down.sql
@@ -0,0 +1,5 @@
+DROP TABLE users CASCADE ALL;
+DROP TABLE user_sessions CASCADE ALL;
+DROP TABLE user_permissions CASCADE ALL;
+DROP TABLE role_permissions CASCADE ALL;
+DROP TABLE user_roles CASCADE ALL;
