e414fb3f24e43578666b458e70b60d4664b92bb8
Barry Nicholson <nicholso@nicengl9u3.niceng.com>
2022-09-14 19:15:31 -0400

    Added Material UI to the node packages used by the system.

--- frontend/src/types/rust.d.ts
+++ frontend/src/types/rust.d.ts
@@ -1,4 +1,39 @@
 /* This file is generated and managed by tsync */
 
-// (1) run `yarn tsync` to generate this file (make sure you `cd` into the `frontend` folder)
-// (2) alternatively, run `cargo fullstack` which will also generate this file!
+type ID = number
+
+type UTC = Date
+
+interface PaginationParams {
+  page: number
+  page_size: number
+}
+
+interface Todo {
+  id: ID
+  text: string
+  created_at: UTC
+  updated_at: UTC
+}
+
+interface TodoChangeset {
+  text: string
+}
+
+interface FileInfo {
+  id: number
+  key: string
+  name: string
+  url: string | undefined
+}
+
+interface Todo {
+  id: number
+  text: string
+  created_at: Date
+  updated_at: Date
+}
+
+interface TodoJson {
+  text: string
+}
