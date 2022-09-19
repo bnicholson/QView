7b6e363243ca22f95663fabd39b8281ff2ea9384
B. Nicholson <b.nicholson@niceng.com>
2022-09-19 12:36:04 -0400

    Added a Material UI grid to show what a Grid might look like on the home page. This will hopefully get me started on the work for the home page that show the current or future tournaments.

--- frontend/src/containers/Home.tsx
+++ frontend/src/containers/Home.tsx
@@ -1,11 +1,41 @@
+
 import React from 'react'
 import reactLogo from '../images/logo.svg'
 import rustLogo from '../images/logo2.svg'
 import plus from '../images/plus.svg'
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import { Grid } from '@mui/material'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 export const Home = () => {
   return (
+ 
     <div>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item>Q2022 June 23-July 2, 2022
+            xs=8</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+      </Grid>   
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <img src={rustLogo} className="App-logo" alt="rust-logo" />
         <img src={plus} alt="plus" />
