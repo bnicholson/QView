9390e0bb35e9e161c3f614daa9dfc2d07c60b7a6
Barry Nicholson <nicholso@nicengl9u3.niceng.com>
2022-09-18 21:51:46 -0400

    Added code to install a Drawer on the left side of the screen.  I'll eventually use this to allow an admin to add features.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,63 +7,136 @@ import { ActivationPage } from './containers/ActivationPage'
 import { RegistrationPage } from './containers/RegistrationPage'
 import { RecoveryPage } from './containers/RecoveryPage'
 import { ResetPage } from './containers/ResetPage'
+import { TournamentPage } from './containers/TournamentPage'
 import React from 'react'
 import './App.css'
 import { Home } from './containers/Home'
 import { Todos } from './containers/Todo'
 import { Files } from './containers/Files'
 import { Route, useNavigate, Routes } from 'react-router-dom'
-import { TournamentPage } from './containers/TournamentPage'
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import { useTheme } from '@emotion/react'
+import styled from '@emotion/styled'
+import { ChevronLeft, ChevronRight, Inbox, Mail } from '@mui/icons-material'
+import { Divider, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 
 if (process.env.NODE_ENV === 'development') import('./setupDevelopment')
-    
+
+const drawerWidth = 240;
+
 const App = () => {
   useAuthCheck()
   const auth = useAuth()
-    
+
   const navigate = useNavigate()
   /* CRA: app hooks */
   const apollo = useApolloClient()
-  
-  // @ts-ignore
-    return (
-    <div className="App">
-      <div className="App-nav-header">
-        <div style={{ display: 'flex', flex: 1 }}>
-          <a className="NavButton" onClick={() => navigate('/')}>Home</a>
-	  <a className="NavButton" onClick={() => navigate('/tournament')}>Tournament</a>
-	  <a className="NavButton" onClick={() => navigate('/todos')}>Todos</a>
-	  <a className="NavButton" onClick={() => navigate('/files')}>Files</a>
-          {/* CRA: left-aligned nav buttons */}
-          <a className="NavButton" onClick={() => navigate('/gql')}>GraphQL</a>
-          <a className="NavButton" onClick={() => navigate('/account')}>Account</a>
 
-        </div>
-        <div>
-          {/* CRA: right-aligned nav buttons */}
-          { auth.isAuthenticated && <a className="NavButton" onClick={() => { auth.logout(); apollo.resetStore(); }}>Logout</a> }
-          { !auth.isAuthenticated && <a className="NavButton" onClick={() => navigate('/login')}>Login/Register</a> }
-        </div>
-      </div>
-      <div style={{ margin: '0 auto', maxWidth: '800px' }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/todos" element={<Todos />} />
-            {/* CRA: routes */}
-            <Route path="/gql" element={<GraphQLPage />} />
-            <Route path="/files" element={<Files />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/recovery" element={<RecoveryPage />} />
-            <Route path="/reset" element={<ResetPage />} />
-            <Route path="/activate" element={<ActivationPage />} />
-            <Route path="/register" element={<RegistrationPage />} />
-            <Route path="/account" element={<AccountPage />} />
-            <Route path="/tournament" element={<TournamentPage />} />	    
-    
-          </Routes>
-      </div>
+  const theme = useTheme();
+
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    /*padding: theme.spacing(0, 1),*/
+    // necessary for content to be below app bar
+/*    ...theme.mixins.toolbar,*/
+    justifyContent: 'flex-end',
+  }));
+
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerClose = () => {
+    if(open==true)
+      setOpen(false);
+    else
+      setOpen(true);
+  };
+
+  // @ts-ignore
+  return (
+    <div className="App">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={() => {handleDrawerClose()}}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={() => handleDrawerClose()}>
+              QView
+            </Typography>
+            <Button color="inherit" onClick={() => alert("button")}>Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeft/>
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <Inbox /> : <Mail />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <Inbox /> : <Mail />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      <div onClick={() => alert("boo hoo")}>Boo Hoo</div>
     </div>
   )
 }
 
 export default App
+
+
