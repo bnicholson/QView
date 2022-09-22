fef4b73632b6bea7be3be6a3b5ffa46c3daed90c
B. Nicholson <b.nicholson@niceng.com>
2022-09-21 21:35:57 -0400

    First step in creating the tournament table.  This is a breaking change. The pipeline will break.  Need to fix this break quickly.

--- migrations/2022-09-21-173540_create_tournament_table/up.sql
+++ migrations/2022-09-21-173540_create_tournament_table/up.sql
@@ -1,1 +1,13 @@
--- Your SQL goes here
+create table tournaments {
+    tournament varchar(32) primary key,
+    fromdate  
+    todate
+    venue
+    city
+    region
+    country
+    contact
+    contactemail 
+    hide as boolean, 
+
+}
