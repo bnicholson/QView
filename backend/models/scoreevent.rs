6cccce4c11e9aaedd84b3504d2d975d77303c355
B. Nicholson <b.nicholson@niceng.com>
2022-10-07 22:44:38 -0400

    Still working on the scoreevent logic. Also cleaned up some dead comment code in models/tournament.rs

--- backend/models/scoreevent.rs
+++ backend/models/scoreevent.rs
@@ -48,82 +48,41 @@ pub struct GameChangeset {
     pub ruleset: String,
 }
 
-#[tsync::tsync]
-#[derive(
-Debug,
-Serialize,
-Deserialize,
-Clone,
-Queryable,
-Insertable,
-Identifiable,
-AsChangeset,
-)]
-#[diesel(table_name=quizzes)]
-#[diesel(primary_key(tdrri, question, eventnum))]
-pub struct Quizzes {
-    pub tdrri: String, 
-    pub question: i8,
-    pub eventnum: i8,
-    pub name: String,
-    pub team: i8,
-    pub quizzer: i8,
-    pub event: String,
-    pub parm1: String,
-    pub parm2: String,
-    pub ts: UTC,
-    pub md5digest: String,
-}
-
-#[tsync::tsync]
-#[derive(Debug, Serialize, Deserialize, Clone, Insertable, AsChangeset)]
-#[diesel(table_name=Quizzes)]
-pub struct QuizzesChangeset {   
-    pub org: String,
-    pub tournament: String,
-    pub division: String,
-    pub room: String,
-    pub round: String,
-    pub key4server: String,
-    pub ignore: bool,
-    pub ruleset: String,
-}
-
 pub fn create(db: &mut Connection, item: &GameChangeset) -> QueryResult<Game> {
     use crate::schema::games::dsl::*;
 
     insert_into(games).values(item).get_result::<Game>(db)
 }
 
-//pub fn read(db: &mut Connection, item_id: BigId) -> QueryResult<Game> {
-//    use crate::schema::games::dsl::*;
-//
-//    games.filter(id.eq(item_id)).first::<Game>(db)
-//}
-
-//pub fn read_all(db: &mut Connection, pagination: &PaginationParams) -> QueryResult<Vec<Game>> {
-//    use crate::schema::games::dsl::*;
-//
-//    let values = games
-//        .order(created_at)
-//        .limit(pagination.page_size)
-//        .offset(
-//            pagination.page
-//                * std::cmp::max(pagination.page_size, PaginationParams::MAX_PAGE_SIZE as i64),
-//        )
-//        .load::<Game>(db);
-//}
-
-pub fn update(db: &mut Connection, item_id: BigId, item: &GameChangeset) -> QueryResult<Game> {
+pub fn read(db: &mut Connection, item_id: String) -> QueryResult<Game> {
     use crate::schema::games::dsl::*;
 
-    diesel::update(games.filter(id.eq(item_id)))
+    games.filter(tdrri.eq(item_id)).first::<Game>(db)
+}
+
+pub fn read_all(db: &mut Connection, pagination: &PaginationParams) -> QueryResult<Vec<Game>> {
+    use crate::schema::games::dsl::*;
+
+    let values = games
+        .order(tdrri)
+        .limit(pagination.page_size)
+        .offset(
+            pagination.page
+                * std::cmp::max(pagination.page_size, PaginationParams::MAX_PAGE_SIZE as i64),
+        )
+        .load::<Game>(db);
+}
+
+pub fn update(db: &mut Connection, item_id: String, item: &GameChangeset) -> QueryResult<Game> {
+    use crate::schema::games::dsl::*;
+
+    diesel::update(games.filter(tdrri.eq(item_id)))
         .set(item)
         .get_result(db)
 }
 
-//pub fn delete(db: &mut Connection, item_id: BigId) -> QueryResult<usize> {
-//    use crate::schema::games::dsl::*;
-//
-//    diesel::delete(games.filter(id.eq(item_id))).execute(db)
-//}
+pub fn delete(db: &mut Connection, item_id: String) -> QueryResult<usize> {
+    use crate::schema::games::dsl::*;
+
+    diesel::delete(games.filter(tdrri.eq(item_id))).execute(db)
+}
