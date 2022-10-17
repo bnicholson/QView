a73d7d250b4410aa8aa53ed7a4428229b2673e80
B. Nicholson <b.nicholson@niceng.com>
2022-10-17 11:18:31 -0400

    Added a theme to App.tsx to enable changing of the default colors.  Teal for instance is now the primary.  The theming logic should be put into a file call theme.tsx eventually.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,26 +15,35 @@ import { Todos } from './containers/Todo'
 import { Files } from './containers/Files'
 import { Route, useNavigate, Routes } from 'react-router-dom'
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import '@mui/material/colors'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu'
 import Drawer from '@mui/material/Drawer'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import { useTheme } from '@emotion/react'
 import styled from '@emotion/styled'
-import { ChevronLeft, ChevronRight, Inbox, Mail, AccountCircle } from '@mui/icons-material'
+import { ChevronLeft, Inbox, Mail, AccountCircle } from '@mui/icons-material'
 import { Breadcrumbs, Divider, Link, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material'
 
 if (process.env.NODE_ENV === 'development') import('./setupDevelopment')
 
 const drawerWidth = 240;
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#008080"
+    }
+  }
+})
 
 const App = () => {
   useAuthCheck()
@@ -44,14 +53,14 @@ const App = () => {
   /* CRA: app hooks */
   const apollo = useApolloClient()
 
-  const theme = useTheme();
+  //const theme = useTheme();
 
   const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
-    /*padding: theme.spacing(0, 1),*/
+    //padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
-    /*    ...theme.mixins.toolbar,*/
+    //    ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
   }));
 
@@ -67,196 +76,200 @@ const App = () => {
 
   // @ts-ignore
   return (
-    <div className="App">
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              onClick={() => { handleDrawerClose() }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" onClick={() => { navigate("/") }}>QView</Typography>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={() => handleDrawerClose()}>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                onClick={() => { handleDrawerClose() }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" onClick={() => { navigate("/") }}>QView</Typography>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 2 }} onClick={() => handleDrawerClose()}>
 
-            </Typography>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              {/* CRA: right-aligned nav buttons */}
-              {auth.isAuthenticated && <a onClick={() => { auth.logout(); apollo.resetStore(); }}>Logout</a>}
-              {!auth.isAuthenticated && <button onClick={() => navigate('/login')}>Login/Register</button>}
-            </Typography>
-            {auth.isAuthenticated && <IconButton> <AccountCircle onClick={() => navigate('/account')} /></IconButton>}
-          </Toolbar>
-        </AppBar>
-        <div>
-          <Breadcrumbs aria-label="breadcrumb">
-            <Link underline="hover" color="inherit" href="/">
-              Home
-            </Link>
-            <Link underline="hover" color="inherit" href="/t/q2022">
-              Q2022
-            </Link>
-            <Link
-              underline="hover"
-              color="inherit"
-              href="/t/q2022/district%20novice"
-            >
-              District Novice
-            </Link>
-            <Typography color="text.primary">Teams</Typography>
-          </Breadcrumbs>
-        </div>
-      </Box>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
+              </Typography>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                {/* CRA: right-aligned nav buttons */}
+                {auth.isAuthenticated && <a onClick={() => { auth.logout(); apollo.resetStore(); }}>Logout</a>}
+                {!auth.isAuthenticated && <button onClick={() => navigate('/login')}>Login/Register</button>}
+              </Typography>
+              {auth.isAuthenticated && <IconButton> <AccountCircle onClick={() => navigate('/account')} /></IconButton>}
+            </Toolbar>
+          </AppBar>
+          <div>
+            <Breadcrumbs aria-label="breadcrumb">
+              <Link underline="hover" color="inherit" href="/">
+                Home
+              </Link>
+              <Link underline="hover" color="inherit" href="/t/q2022">
+                Q2022
+              </Link>
+              <Link
+                underline="hover"
+                color="inherit"
+                href="/t/q2022/district%20novice"
+              >
+                District Novice
+              </Link>
+              <Typography color="text.primary">Teams</Typography>
+            </Breadcrumbs>
+          </div>
+        </Box>
+        <Drawer
+          sx={{
             width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeft />
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {['Tournament', 'Division', 'Room', 'Round'].map((text, index) => (
-            <ListItem key={text} disablePadding
-              onClick={() => {
-                switch (index % 4) {
-                  case 0:
-                    navigate("/tournament");
-                    break;
-                  case 1:
-                    alert('division');
-                    break;
-                  case 2:
-                    alert("room");
-                    break;
-                  case 3:
-                    alert('round');
-                }
-              }}>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <Inbox /> : <Mail />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['Quizzes', 'Team', 'Individual'].map((text, index) => (
-            <ListItem key={text} disablePadding
-              onClick={() => {
-                switch (index % 3) {
-                  case 0:
-                    alert("quizzes");
-                    break;
-                  case 1:
-                    alert("team");
-                    break;
-                  case 2:
-                    alert("individual");
-                    break;
-                }
-              }}>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <Inbox /> : <Mail />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['Todos', 'Files', 'GraphQL'].map((text, index) => (
-            <ListItem key={text} disablePadding
-              onClick={() => {
-                switch (index % 3) {
-                  case 0:
-                    navigate("/todos");
-                    break;
-                  case 1:
-                    navigate("/files");
-                    break;
-                  case 2:
-                    navigate("/gql");
-                    break;
-                }
-              }}
-            >
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <Inbox /> : <Mail />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <div style={{ margin: '0 auto', maxWidth: '800px' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todos" element={<Todos />} />
-          {/* CRA: routes */}
-          <Route path="/gql" element={<GraphQLPage />} />
-          <Route path="/files" element={<Files />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/recovery" element={<RecoveryPage />} />
-          <Route path="/reset" element={<ResetPage />} />
-          <Route path="/activate" element={<ActivationPage />} />
-          <Route path="/register" element={<RegistrationPage />} />
-          <Route path="/account" element={<AccountPage />} />
-          <Route path="/tournament" element={<Tournaments />} />
-        </Routes>
-      </div>
-      <Box textAlign="center" pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }}>
-        </Box>
-      <Box px={{ xs: 3, sm: 10 }} py={{ xs: 5, sm: 10 }} bgcolor="text.secondary" color="white">
-        <Container maxWidth='lg'>
-          <Grid container spacing={5}>
-            <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Help</Box>
-              <Box>Contact</Box>
-              <Box>Support</Box>
-              <Box>Privacy</Box>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Help</Box>
-              <Box>Login</Box>
-              <Box>Register</Box>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Messages</Box>
-              <Box>Backup</Box>
-              <Box>History</Box>
-              <Box>Roll</Box>
-            </Grid>
-          </Grid>
-        </Container>
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeft />
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            {['Tournament', 'Division', 'Room', 'Round'].map((text, index) => (
+              <ListItem key={text} disablePadding
+                onClick={() => {
+                  switch (index % 4) {
+                    case 0:
+                      navigate("/tournament");
+                      break;
+                    case 1:
+                      alert('division');
+                      break;
+                    case 2:
+                      alert("room");
+                      break;
+                    case 3:
+                      alert('round');
+                  }
+                }}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <Inbox /> : <Mail />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['Quizzes', 'Team', 'Individual'].map((text, index) => (
+              <ListItem key={text} disablePadding
+                onClick={() => {
+                  switch (index % 3) {
+                    case 0:
+                      alert("quizzes");
+                      break;
+                    case 1:
+                      alert("team");
+                      break;
+                    case 2:
+                      alert("individual");
+                      break;
+                  }
+                }}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <Inbox /> : <Mail />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['Todos', 'Files', 'GraphQL'].map((text, index) => (
+              <ListItem key={text} disablePadding
+                onClick={() => {
+                  switch (index % 3) {
+                    case 0:
+                      navigate("/todos");
+                      break;
+                    case 1:
+                      navigate("/files");
+                      break;
+                    case 2:
+                      navigate("/gql");
+                      break;
+                  }
+                }}
+              >
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <Inbox /> : <Mail />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <div style={{ margin: '0 auto', maxWidth: '800px' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/todos" element={<Todos />} />
+            {/* CRA: routes */}
+            <Route path="/gql" element={<GraphQLPage />} />
+            <Route path="/files" element={<Files />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/recovery" element={<RecoveryPage />} />
+            <Route path="/reset" element={<ResetPage />} />
+            <Route path="/activate" element={<ActivationPage />} />
+            <Route path="/register" element={<RegistrationPage />} />
+            <Route path="/account" element={<AccountPage />} />
+            <Route path="/tournament" element={<Tournaments />} />
+          </Routes>
+        </div>
         <Box textAlign="center" pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }}>
-          QView by QuizStuff &reg; {new Date().getFullYear()}
         </Box>
-      </Box>
-    </div >
+        <Box px={{ xs: 3, sm: 10 }} py={{ xs: 5, sm: 10 }} bgcolor="text.secondary" color="white">
+          <Typography variant="h6">
+            <Container maxWidth='lg'>
+              <Grid container spacing={5}>
+                <Grid item xs={12} sm={4}>
+                  <Box borderBottom={1}>Help</Box>
+                  <Box>Contact</Box>
+                  <Box>Support</Box>
+                  <Box>Privacy</Box>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Box borderBottom={1}>Help</Box>
+                  <Box>Login</Box>
+                  <Box>Register</Box>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Box borderBottom={1}>Messages</Box>
+                  <Box>Backup</Box>
+                  <Box>History</Box>
+                  <Box>Roll</Box>
+                </Grid>
+              </Grid>
+            </Container>
+            <Box textAlign="center" pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }}>
+              QView by QuizStuff &reg; 2022-{new Date().getFullYear()}
+            </Box>
+          </Typography>
+        </Box>
+      </div >
+    </ThemeProvider>
   )
 }
 
