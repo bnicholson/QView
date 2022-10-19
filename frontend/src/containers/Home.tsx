60807acc6789876561e9549abbea74d6bc8e0d3a
B. Nicholson <b.nicholson@niceng.com>
2022-10-18 22:20:25 -0400

    Still looking at the way to display tournaments on the front page.

--- frontend/src/containers/Home.tsx
+++ frontend/src/containers/Home.tsx
@@ -73,6 +73,17 @@ export const Home = () => {
               Quizzing occurs in round robin or pool play format with multiple division
               tournaments after round robin play has concluded.  Top teams in each
               tournament are recognized as well as Top 20 Individuals.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus 
+              non enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus 
+              imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. 
+              Convallis convallis tellus id interdum velit laoreet id donec ultrices. 
+              Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit 
+              adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra 
+              nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. 
+              Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat 
+              vivamus at augue. At augue eget arcu dictum varius duis at consectetur lorem. 
+              Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
             </Typography>
           </CardContent>
         </Box>
