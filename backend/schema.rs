5cd71f5a5b76c2fa972ebbfcf0ea2faa6aeb6b05
B. Nicholson <b.nicholson@niceng.com>
2024-04-03 11:23:38 -0400

    Fixed some migrations that were causing issues with creating a fresh postgresql database.

--- backend/schema.rs
+++ backend/schema.rs
@@ -4,8 +4,10 @@ diesel::table! {
     apicalllog (apicallid) {
         created_at -> Timestamptz,
         apicallid -> Int8,
+        #[max_length = 8]
         method -> Varchar,
         uri -> Text,
+        #[max_length = 32]
         version -> Varchar,
         headers -> Text,
     }
@@ -46,9 +48,12 @@ diesel::table! {
     divisions (did) {
         did -> Int8,
         tid -> Int8,
+        #[max_length = 32]
         dname -> Varchar,
+        #[max_length = 32]
         breadcrumb -> Varchar,
         hide -> Bool,
+        #[max_length = 1024]
         shortinfo -> Varchar,
         created_at -> Timestamptz,
         updated_at -> Timestamptz,
@@ -59,25 +64,41 @@ diesel::table! {
     eventlogs (evid) {
         evid -> Int8,
         created_at -> Timestamptz,
+        #[max_length = 64]
         clientkey -> Varchar,
+        #[max_length = 48]
         organization -> Varchar,
+        #[max_length = 32]
         bldgroom -> Varchar,
+        #[max_length = 48]
         tournament -> Varchar,
+        #[max_length = 48]
         division -> Varchar,
+        #[max_length = 48]
         room -> Varchar,
+        #[max_length = 48]
         round -> Varchar,
         question -> Int4,
         eventnum -> Int4,
+        #[max_length = 64]
         name -> Varchar,
         team -> Int4,
         quizzer -> Int4,
+        #[max_length = 2]
         event -> Varchar,
+        #[max_length = 64]
         parm1 -> Varchar,
+        #[max_length = 64]
         parm2 -> Varchar,
+        #[max_length = 32]
         ts -> Varchar,
+        #[max_length = 32]
         clientip -> Varchar,
+        #[max_length = 32]
         md5digest -> Varchar,
+        #[max_length = 80]
         nonce -> Varchar,
+        #[max_length = 32]
         s1s -> Varchar,
     }
 }
@@ -85,13 +106,20 @@ diesel::table! {
 diesel::table! {
     games (gid) {
         gid -> Int8,
+        #[max_length = 48]
         org -> Varchar,
+        #[max_length = 48]
         tournament -> Varchar,
+        #[max_length = 48]
         division -> Varchar,
+        #[max_length = 48]
         room -> Varchar,
+        #[max_length = 48]
         round -> Varchar,
+        #[max_length = 64]
         clientkey -> Varchar,
         ignore -> Bool,
+        #[max_length = 32]
         ruleset -> Varchar,
     }
 }
@@ -101,14 +129,19 @@ diesel::table! {
         gid -> Int8,
         question -> Int4,
         eventnum -> Int4,
+        #[max_length = 64]
         name -> Varchar,
         team -> Int4,
         quizzer -> Int4,
+        #[max_length = 2]
         event -> Varchar,
+        #[max_length = 8]
         parm1 -> Varchar,
+        #[max_length = 8]
         parm2 -> Varchar,
         clientts -> Timestamptz,
         serverts -> Timestamptz,
+        #[max_length = 32]
         md5digest -> Varchar,
     }
 }
@@ -125,9 +158,13 @@ diesel::table! {
     rooms (roomid) {
         roomid -> Int8,
         tid -> Int8,
+        #[max_length = 32]
         name -> Varchar,
+        #[max_length = 32]
         building -> Varchar,
+        #[max_length = 64]
         quizmaster -> Varchar,
+        #[max_length = 64]
         contentjudge -> Varchar,
         comments -> Text,
     }
@@ -138,16 +175,27 @@ diesel::table! {
         sid -> Int8,
         tid -> Int8,
         roundtime -> Timestamptz,
+        #[max_length = 32]
         tournament -> Varchar,
+        #[max_length = 32]
         division -> Varchar,
+        #[max_length = 32]
         room -> Varchar,
+        #[max_length = 32]
         round -> Varchar,
+        #[max_length = 32]
         team1 -> Nullable<Varchar>,
+        #[max_length = 32]
         team2 -> Nullable<Varchar>,
+        #[max_length = 32]
         team3 -> Nullable<Varchar>,
+        #[max_length = 32]
         quizmaster -> Nullable<Varchar>,
+        #[max_length = 32]
         contentjudge -> Nullable<Varchar>,
+        #[max_length = 32]
         scorekeeper -> Nullable<Varchar>,
+        #[max_length = 1024]
         stats -> Nullable<Varchar>,
     }
 }
@@ -164,18 +212,28 @@ diesel::table! {
 diesel::table! {
     tournaments (tid) {
         tid -> Int8,
+        #[max_length = 32]
         organization -> Varchar,
+        #[max_length = 32]
         tname -> Varchar,
+        #[max_length = 32]
         breadcrumb -> Varchar,
         fromdate -> Date,
         todate -> Date,
+        #[max_length = 64]
         venue -> Varchar,
+        #[max_length = 64]
         city -> Varchar,
+        #[max_length = 64]
         region -> Varchar,
+        #[max_length = 32]
         country -> Varchar,
+        #[max_length = 64]
         contact -> Varchar,
+        #[max_length = 255]
         contactemail -> Varchar,
         hide -> Bool,
+        #[max_length = 1024]
         shortinfo -> Varchar,
         info -> Text,
         created_at -> Timestamptz,
